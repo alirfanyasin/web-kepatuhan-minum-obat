45ecaf48142d4eef715d4bcf16f938039ec6132e
Irfan Yasin <irfanyasin1303@gmail.com>
2026-03-08 12:39:49 +0700

    feat: create settings view

---
+++ resources/views/pages/app/settings.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+  <x-breadcrumb title="Pengaturan" sub-title="Data akun dan profile" />
+
+  <div class="grid lg:grid-cols-2 grid-cols-1 gap-4">
+      <section
+        class="p-5 bg-white dark:bg-surface-900 rounded-2xl border border-surface-200 dark:border-surface-800 mb-2 animate-slide-up">
+        
+      </section>
+  </div>
+@endsection
